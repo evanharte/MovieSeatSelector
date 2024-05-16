c4a7b23401937ddb1d2d1828945dae7d1f243f0f
Evan Harte <126525806+evanharte@users.noreply.github.com>
2024-05-16 20:18:40 -0230

    updated to meet assignment requirements.

--- MovieSeatSelector.java
+++ MovieSeatSelector.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class MovieSeatSelector {
     // Create a java class that allows the user to reserve an available seat in a movie theater.
-    int[][] theatreSeats = new int[8][6];
+    String[][] theatreSeats = new String[8][6];
 
     public MovieSeatSelector() {
         for (int row = 0; row < theatreSeats.length; row++) {
             for (int col = 0; col < theatreSeats[0].length; col++) {
-                theatreSeats[row][col] = 0;
+                theatreSeats[row][col] = "O";
             }
         }
     }
@@ -27,7 +26,13 @@ public class MovieSeatSelector {
     }
 
     public void claimSeat(int row, int col) {
-        theatreSeats[row - 1][col - 1] = 1;
+        if (theatreSeats[row - 1][col - 1].equals("X")) {
+            System.out.println();
+            System.out.println("The seat at " + row + ", " + col + " has already been claimed. Please try again.");
+        } else {
+            theatreSeats[row - 1][col - 1] = "X";
+            System.out.println("Seat successfully claimed (indicated by 'X').");
+        }
     }
 
     public static void main(String[] args) {
@@ -49,9 +54,8 @@ public class MovieSeatSelector {
                 int row = scanner.nextInt();
                 System.out.println("Enter the column/seat number (1-6): ");
                 int col = scanner.nextInt();
+
                 seatSelector.claimSeat(row, col);
-                System.out.println();
-                System.out.println("Seat successfully claimed (indicated by '1').");
                 System.out.println();
                 seatSelector.displaySeats();
                 System.out.println();
